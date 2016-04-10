15846e38af1e072e4def39e84e2fc4cf867fbad7
Leo Yao <leo.yao.dev@gmail.com>
2016-04-10 10:53:07 -0400

    add transaction management

--- sqlite/src/sqlite_utils.c
+++ sqlite/src/sqlite_utils.c
@@ -13,3 +13,35 @@ int insert_directory(sqlite3* db1, directory* data){
 	return 0;
 }
 
+int begin_transaction(sqlite3* db){
+	char* sql_begin = "BEGIN TRANSACTION; UPDATE LOCK SET dummy = 1;";
+	char *zErrMsg = 0;
+	//Begin transaction to database
+	int rc = sqlite3_exec(db, sql_begin, 0, 0, &zErrMsg);
+	//If SQLITE is busy, retry twice, if still busy then abort
+	for(int i=0;i<2;i++){
+		if(rc == SQLITE_BUSY){
+			delay(50);
+			rc = sqlite3_exec(db, sql_begin, 0, 0, &zErrMsg);
+		}else{
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+int commit_transaction(sqlite3* db){
+
+	char *zErrMsg = 0;
+	int rc = sqlite3_exec(db, "Commit Transaction;", 0, 0, &zErrMsg);
+
+	return rc;
+}
+
+int rollback_transaction(sqlite3* db){
+	char *zErrMsg = 0;
+	int rc = sqlite3_exec(db, "Rollback Transaction;", 0, 0, &zErrMsg);
+
+	return rc;
+}
